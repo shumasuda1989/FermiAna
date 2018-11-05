58f0bacde03d7b3d2c9218879f484054d436fb30
shumasuda1989 <shu.masuda1989@gmail.com>
2018-11-05 15:56:31 +0900

    Fix Bayesian UL calc bug. + Update a few scripts

--- MakeTSmapFromLike.py
+++ MakeTSmapFromLike.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 
+'''
+MakeTSmapFromLike.py
+ 1. Prepare a map FITS file which has the same header as your desired TS map and Rename it to the same as the environment variable 'tsmap'.
+ 2. Create a XML file which includes 'putative source' (point source)
+   with (RA,DEC) values of e.g. (%f,%f) and with its power law Prefactor freed.
+ 3. Set i.e. export environment variables necessary for Likelihood.py 
+   and gttsmap.
+ 4. Then, run this script.
+'''
+
 import os
 from time import sleep
 import subprocess
 import numpy as np
 import pyfits
 from pywcs import WCS
-from ReadResult import ReadResult
+
 
 env=os.environ
 
@@ -64,16 +74,11 @@ if not os.path.isdir(lockdir):
     os.mkdir(lockdir)
 if not os.path.isdir(filedir):
     os.mkdir(filedir)
-
+resultstmp=env.get('results','results.dat')
 with pyfits.open(env['tsmap']) as fi:
     wh=np.where(fi[0].data==0)
 
 if env.get('SKIP','') == '':
-
-    with open(env['srcmdlin']) as f:
-        lines=f.read()
-        lines=lines.replace('</source_library>\n','')
-        lines=lines.replace('</source_library>','')
 
     # for i in range(nxpix/Npix):
     #     for j in range(nypix/Npix):
@@ -81,23 +86,19 @@ if env.get('SKIP','') == '':
     for i,j in zip(wh[0],wh[1]):
         print i,j,'RA:',RA[i,j], 'DEC:',DEC[i,j]
 
-        testsrc='  <source name="putative source" type="PointSource">\n    <spectrum apply_edisp="false" type="PowerLaw">\n      <parameter free="1" max="10000" min="0.0001" name="Prefactor" scale="1e-17" value="1.00" />\n      <parameter free="1" max="10" min="0" name="Index" scale="-1" value="2.0" />\n      <parameter free="0" max="500000" min="30" name="Scale" scale="1" value="10000" />\n    </spectrum>\n    <spatialModel type="SkyDirFunction">\n      <parameter free="0" max="360" min="-360" name="RA" scale="1" value="%f" />\n      <parameter free="0" max="90" min="-90" name="DEC" scale="1" value="%f" />\n    </spatialModel>\n  </source>\n</source_library>\n' % (RA[i,j],DEC[i,j])
-
         lock='%s/lock_%d_%d'%(lockdir,i,j)
         log='%s/log_%d_%d.txt'%(filedir,i,j)
         xmlin='%s/%s'%(filedir,env['srcmdlin'].replace('.xml','_%d_%d.xml'%(i,j)))
         xmlout='%s/%s'%(filedir,env['srcmdlout'].replace('.xml','_%d_%d.xml'%(i,j)))
-        results='%s/%s'%(filedir,env['results'].replace('.dat','_%d_%d.dat'%(i,j)))
+        results='%s/%s'%(filedir,resultstmp.replace('.dat','_%d_%d.dat'%(i,j)))
         with open(xmlin, mode='w') as f:
-            f.write(lines+testsrc)
+            f.write(lines % (RA[i,j],DEC[i,j]))
 
-        command='subcmd.sh %s "source %s/fermi-init.sh; touch %s; srcmap=%s lvtime=%s bexpcube=%s srcmdlin=%s srcmdlout=%s optimizer=%s METHOD=%s specfile=None results=%s USE_BL_EDISP=%s refit=%s plot=%s slist=\'putative source\' ../Likelihood.py; rm %s" %s %s' %(option[0],env['FERMI_DIR'], lock, env['srcmap'], env['lvtime'], env['bexpcube'], xmlin, xmlout, env['optimizer'], env['METHOD'], results, env['USE_BL_EDISP'], env['refit'], env['plot'], lock, log, option[1])
+        if subprocess.call('type subcmd.sh &>/dev/null', shell=True)!=0:
+            command='(source %s/fermi-init.sh; touch %s; srcmap=%s lvtime=%s bexpcube=%s srcmdlin=%s srcmdlout=%s optimizer=%s METHOD=%s specfile=None results=%s USE_BL_EDISP=%s refit=%s plot=%s SKIP_UL=true ../Likelihood.py; rm %s) &>%s &' %(env['FERMI_DIR'], lock, env['srcmap'], env['lvtime'], env['bexpcube'], xmlin, xmlout, env['optimizer'], env['METHOD'], results, env['USE_BL_EDISP'], env['refit'], env['plot'], lock, log)
+        else:
+            command='subcmd.sh %s "source %s/fermi-init.sh; touch %s; srcmap=%s lvtime=%s bexpcube=%s srcmdlin=%s srcmdlout=%s optimizer=%s METHOD=%s specfile=None results=%s USE_BL_EDISP=%s refit=%s plot=%s SKIP_UL=true ../Likelihood.py; rm %s" %s %s' %(option[0],env['FERMI_DIR'], lock, env['srcmap'], env['lvtime'], env['bexpcube'], xmlin, xmlout, env['optimizer'], env['METHOD'], results, env['USE_BL_EDISP'], env['refit'], env['plot'], lock, log, option[1])
 
-            # if j%2==0:
-            #     cmd1='touch %s; srcmap=%s lvtime=%s bexpcube=%s srcmdlin=%s srcmdlout=%s optimizer=%s METHOD=%s specfile=None results=%s USE_BL_EDISP=%s refit=%s plot=%s slist=\'putative source\' ../Likelihood.py; rm %s' % (lock, env['srcmap'], env['lvtime'], env['bexpcube'], xmlin, xmlout, env['optimizer'], env['METHOD'], results, env['USE_BL_EDISP'], env['refit'], env['plot'], lock)
-
-            # else:
-            #     command='subcmd.sh %s "source %s/fermi-init.sh; %s; touch %s; srcmap=%s lvtime=%s bexpcube=%s srcmdlin=%s srcmdlout=%s optimizer=%s METHOD=%s specfile=None results=%s USE_BL_EDISP=%s refit=%s plot=%s slist=\'putative source\' ../Likelihood.py; rm %s" %s %s' %(option[0],env['FERMI_DIR'], cmd1, lock, env['srcmap'], env['lvtime'], env['bexpcube'], xmlin, xmlout, env['optimizer'], env['METHOD'], results, env['USE_BL_EDISP'], env['refit'], env['plot'], lock, log, option[1])
                 # print command
                 # exit(0)
         subprocess.call(command, shell=True)
@@ -115,11 +116,12 @@ data=np.full((nxpix,nypix),-1)
 
 for i in range(nxpix/Npix):
     for j in range(nypix/Npix):
-        results='%s/%s'%(filedir,env['results'].replace('.dat','_%d_%d.dat'%(i,j)))
+        results='%s/%s'%(filedir,resultstmp.replace('.dat','_%d_%d.dat'%(i,j)))
         xmlout='%s/%s'%(filedir,env['srcmdlout'].replace('.xml','_%d_%d.xml'%(i,j)))
         if os.path.isfile(results) and os.path.isfile(xmlout):
             print '%d_%d'%(i,j)
-            res,_=ReadResult(results)
+            with open(results) as f:
+                res=eval(f.read())
             data[Npix*i:Npix*(i+1),Npix*j:Npix*(j+1)]=res['putative source']['TS value']
             if data[Npix*i:Npix*(i+1),Npix*j:Npix*(j+1)][0]==0:
                 print '=0'
