0829dd59a460a1441641247e7a86939390c41dd4
shumasuda1989 <shu.masuda1989@gmail.com>
2018-11-24 03:00:33 +0900

    Update makedata*.sh scripts

--- makedataDiff.sh
+++ makedataDiff.sh
@@ -2,7 +2,8 @@
 
 curdir=$PWD
 srcname=GammaCygni
-export srcname=${srcname} method=binned npix1=450 npix2=600 binsz=0.06666666667 ## MUST be same as the settings in makedataFR.sh
+export srcname=${srcname} method=binned npix1=450 npix2=600
+## MUST be same as the settings in makedataFR.sh except energies
 
 E=(5000 10772 23208 50000 107722 232079 500000)
 nE=${#E[*]}
@@ -12,11 +13,11 @@ nEbin=$((nE-1))
 Diffdir=DataPoints
 relFRdir=../.. ##relative path of $curdir relative to $workdir
 
-targetxml="$relFRdir/${srcname}_output_model.xml"
+targetxml=$relFRdir/${srcname}_output_model.xml
 
 slist="gamma Cygni,3FGL J2021.5+4026"
 export slist="$slist"
-export docut=1 filetobecut=$srcname.fits skip=1 skipgttsmap=1 skipres=3
+export docut=1 filetobecut=$relFRdir/${srcname}_binned.fits skip=1 skipgttsmap=1 skipres=3
 # export skip=7 skipgttsmap=1 skipres=3
 
 for ((i0=0 ; i0<nEbin ; i0++))
@@ -31,7 +32,6 @@ do
     cd $workdir || (echo error cd && exit 1)
 
     ln -fs $relFRdir/L*SC*.fits . || (echo error ln; exit 1)
-    ln -fs $relFRdir/${srcname}.fits || (echo error ln; exit 1)
     ln -fs $relFRdir/${srcname}_binned_ltcube.fits || (echo error ln; exit 1)
 
     ecenter=$(echo "scale=9;sqrt(${E[$i0]}*${E[$i1]})" | bc)
--- makedataFR.sh
+++ makedataFR.sh
@@ -5,16 +5,16 @@ date
 echo
 
 export srcname=${srcname} method=binned npix1=450 npix2=600 #mandatory
-## Uncomment below line and add parameters if you need
-# export binsz= Ra= Dec= npix3= (for tsmap) emin= emax= enumbins= proj= ptsrc=
+## Add below parameters if you need
+# export binsz=(default:0.2) Ra= Dec= npix3=(for tsmap) emin= emax= enumbins= proj= ptsrc=
 
 export srcmdlout=${srcname}_output_model.xml
 
-## Uncomment below line if you want an upper limit value (and SED) of interested sources
+## Uncomment below line and Add the names of sources you are interested in if you want upper limit values (and SED) of them
 # export slist="gamma Cygni,3FGL J2021.5+4021" 
 
-## For initial run. If you have a event file which should be cut by gtselect, specify 'filetobecut', else i.e. you have only L*PH??.fits photon files, you don't need 'filetobecut'.
-# export docut=1 filetobecut=$srcname.fits skipmkmodel=1 skipgttsmap=1
+export docut=1 skipmkmodel=1 skipgttsmap=1
+## For initial run. If you have a event file which should be cut by gtselect in advance, specify 'filetobecut', else i.e. you have only L*PH??.fits photon files, you don't need 'filetobecut'. Put or link them into work dir or Make list.txt
 
 ## Uncomment below line if you want to run only pyLikelihood
 # export skip=7 skipmkmodel=1 skipgttsmap=1 skipres=3
@@ -24,7 +24,7 @@ export srcmdlout=${srcname}_output_model.xml
 if [ -z "$export" ]; then
     ../execFermiTools.sh
 else
-    source ../execFermiTools.sh #for only getting environment variables
+    source ../execFermiTools.sh #only for getting environment variables
 fi
 
 echo
