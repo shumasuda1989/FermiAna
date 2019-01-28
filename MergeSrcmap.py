4ef064c11332cf75720c94b30f6ea913355214bb
shumasuda1989 <shu.masuda1989@gmail.com>
2019-01-28 21:06:08 +0900

    minor update

--- MergeSrcmap.py
+++ MergeSrcmap.py
@@ -3,19 +3,18 @@
 import sys
 try:
     import pyfits
-    ow={'clobber':True}
+    ow=dict(clobber=True)
 except ImportError:
     import astropy.io.fits as pyfits
-    ow={'overwrite':True}
+    ow=dict(overwrite=True)
 
-f1=pyfits.open(sys.argv[1])
-f2=pyfits.open(sys.argv[2])
+with pyfits.open(sys.argv[1]) as f1, pyfits.open(sys.argv[2]) as f2:
 
-for hdu in f2[3:]:
-    f1.append(hdu)
+    for hdu in f2[3:]:
+        f1.append(hdu)
 
-if len(sys.argv) > 3:
-    f1.writeto(sys.argv[3],**ow)
-else:
-    f1.writeto(sys.argv[1],**ow)
+    if len(sys.argv) > 3:
+        f1.writeto(sys.argv[3],**ow)
+    else:
+        f1.writeto(sys.argv[1],**ow)
 
